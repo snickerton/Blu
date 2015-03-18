765a5730610bca39ccca477f43bb76822f0a9cb5
snickerton <snickerton@users.noreply.github.com>
2015-03-18 18:49:08 -0500

    Update blu.js

--- blu.js
+++ blu.js
@@ -1,7 +1,8 @@
 
 
 window.onload=function(){
-	var ctx = document.getElementById("canvas").getContext("2d");
+	var canvas = document.getElementsById("canvas");
+	var ctx = canvas.getContext("2d");
 	//ctx.fillRect(0,0,ctx.width,ctx.height); 
 	var i = 0;
 	setInterval(function(){
@@ -12,13 +13,13 @@ window.onload=function(){
 		ctx.fillStyle = "#FFF";
 		if(i == 0){
 			console.log("scenario 0");
-			ctx.fillText("Loading.",ctx.width/2,ctx.height/2);
+			ctx.fillText("Loading.",canvas.width/2,canvas.height/2);
 		}
 		if(i == 1){
-			ctx.fillText("Loading..",ctx.width/2,ctx.height/2);
+			ctx.fillText("Loading..",canvas.width/2,canvas.height/2);
 		}
 		if(i == 2){
-			ctx.fillText("Loading...",ctx.width/2,ctx.height/2);
+			ctx.fillText("Loading...",canvas.width/2,canvas.height/2);
 		}
 		i = (i+1)%3;
 	}, 1000);
