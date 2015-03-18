5c671fd421a69380394000f03bfbac8677e8c49f
snickerton <snickerton@users.noreply.github.com>
2015-03-18 18:01:57 -0500

    Update blu.js

--- blu.js
+++ blu.js
@@ -1,6 +1,5 @@
 
- var canvas = document.getElementById("canvas");
- var ctx = canvas.getContext("2d");
+ctx = document.getElementById("canvas").getContext("2d");
 
 window.onload(function(){
 		drawLoadingScreen();
