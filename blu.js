cf61ba7d9360d3c716456a2a6f9270878e6b676f
snickerton <snickerton@users.noreply.github.com>
2015-03-18 17:59:19 -0500

    Update blu.js

--- blu.js
+++ blu.js
@@ -1,1 +1,24 @@
 
+var ctx = document.getElementById("canvas").getContext("2d");
+
+window.onload(function(){
+		drawLoadingScreen();
+});
+
+
+function drawLoadingScreen(){
+	var i = 0;
+	setInterval(function(){
+		ctx.font = "30px Arial";
+		if(i == 0){	
+			ctx.fillText("Loading.",ctx.width/2,ctx.height/2);
+		}
+		if(i == 1){
+			ctx.fillText("Loading..",ctx.width/2,ctx.height/2);
+		}
+		if(i == 2){
+			ctx.fillText("Loading...",ctx.width/2,ctx.height/2);
+		}
+		i = (i+1)%3;
+	}, 1000);
+}
