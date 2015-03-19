4438f66c0d83fc0e72df7646ece0266b65e554fa
snickerton <snickerton@users.noreply.github.com>
2015-03-18 19:04:08 -0500

    Update blu.js

--- blu.js
+++ blu.js
@@ -1,9 +1,10 @@
 
+var canvas, ctx, cWidth, cHeight;
+
 
 window.onload=function(){
-	var canvas = document.getElementById("canvas");
-	var ctx = canvas.getContext("2d");
-	var cWidth, cHeight;
+	canvas = document.getElementById("canvas");
+	ctx = canvas.getContext("2d");
 	cWidth = canvas.width;
 	cHeight = canvas.height;
 	ctx.fillStyle = "#FFF";
