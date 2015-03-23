c7e9b9adf5ad437bce7b620ccb18dfde793d241b
snickerton <snickerton@users.noreply.github.com>
2015-03-23 09:40:58 -0500

    Update blu.js

--- blu.js
+++ blu.js
@@ -7,20 +7,17 @@ window.onload=function(){
 	ctx = canvas.getContext("2d");
 	cWidth = canvas.width;
 	cHeight = canvas.height;
-	ctx.fillStyle = "#FFF";
-	ctx.fillRect(0,0,cWidth, cHeight);
-	showLS();
+	//showLS();
+	game();
 };
 
 	function showLS(){
 	var i = 0;
 	setInterval(function(){
-		console.log("ran");
-		
 		ctx.font = "30px Arial";
-		ctx.fillStyle = "#FFF";
-		ctx.fillRect(0,0,cWidth, cHeight);
 		ctx.fillStyle = "#000";
+		ctx.fillRect(0,0,cWidth, cHeight);
+		ctx.fillStyle = "#FFF";
 		if(i == 0){
 			console.log("scenario 0");
 			ctx.fillText("Loading.",cWidth/2 - ctx.measureText("Loading.").width/2,cHeight/2);
@@ -32,6 +29,14 @@ window.onload=function(){
 			ctx.fillText("Loading...",cWidth/2 - ctx.measureText("Loading...").width/2,cHeight/2);
 		}
 		i = (i+1)%3;
+	}, 1000);
+}
+
+function game(){
+		setInterval(function(){
+		ctx.fillStyle = "#000";
+		ctx.fillRect(0,0,cWidth, cHeight);
+
 	}, 1000);
 }
 
